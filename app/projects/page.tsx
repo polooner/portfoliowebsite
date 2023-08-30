8793ed3bb242186082a4b561b9cda79019997a18
Filip Wojda <wojdafilipdev@gmail.com>
2023-08-30 12:54:25 -0400

    hydration error

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,94 @@
-import { getCurrentUser } from '@/lib/session';
+'use client';
 
-export default async function Projects() {
-  const user = await getCurrentUser();
+import PostContent from '@/components/PostContent';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@radix-ui/react-dialog';
+import { DialogHeader } from '@/components/ui/dialog';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+});
+
+export default function Projects() {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <main>
       <h1 className='heading'>A collection of my projects.</h1>
-      <p>{user?.email}</p>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>Post</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Post a project</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete
+              your account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='space-y-8'
+            >
+              <FormField
+                control={form.control}
+                name='username'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title of post</FormLabel>
+                    <FormControl>
+                      <Input placeholder='yeah' {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type='submit'>Submit</Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+
       <p className='text-gray-500'>Source code sometimes included.</p>
     </main>
   );
---
+++ components/PostContent.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@radix-ui/react-dialog';
+import { DialogHeader } from './ui/dialog';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { Button } from './ui/button';
+import { useState } from 'react';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+});
+
+interface PostContentProps {
+  edit_type?: string;
+}
+
+const PostContent = (props: PostContentProps) => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button variant='outline'>Post content</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Post a project</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8'
+          >
+            <FormField
+              control={form.control}
+              name='username'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title of post</FormLabel>
+                  <FormControl>
+                    <Input placeholder='yeah' {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PostContent;
