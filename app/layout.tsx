ebbb5318b58bfb632a4fc41e8c2c4a0d25093656
Filip Wojda <wojdafilipdev@gmail.com>
2023-11-28 14:37:05 -0500

    whatever

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from '@vercel/analytics/react';
-import Header from '@/components/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import Footer from '@/components/Footer';
--- components/animated/AnimatedMenuDock.tsx
+++ components/animated/AnimatedMenuDock.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { MoreVertical, LucideIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useState } from 'react';
 import PopoverSocials from '../PopoverSocials';
