fa5cec7330c30ddfe089f938035f6129e42efcf2
Filip Wojda <wojdafilipdev@gmail.com>
2024-01-04 13:13:00 +0100

    og fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -22,8 +22,8 @@ export async function generateMetadata({
     image,
   } = post.metadata;
   let ogImage = image
-    ? `https://leerob.io${image}`
-    : `https://leerob.io/og?title=${title}`;
+    ? `https://filipwojda.dev${image}`
+    : `https://filipwojda.dev/og?title=${title}`;
 
   return {
     title,
@@ -33,7 +33,7 @@ export async function generateMetadata({
       description,
       type: 'article',
       publishedTime,
-      url: `https://leerob.io/blog/${post.slug}`,
+      url: `https://filipwojda.dev/blog/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -102,12 +102,12 @@ export default function Blog({ params }) {
             dateModified: post.metadata.publishedAt,
             description: post.metadata.summary,
             image: post.metadata.image
-              ? `https://leerob.io${post.metadata.image}`
-              : `https://leerob.io/og?title=${post.metadata.title}`,
-            url: `https://leerob.io/blog/${post.slug}`,
+              ? `https://filipwojda.dev${post.metadata.image}`
+              : `https://filipwojda.dev/og?title=${post.metadata.title}`,
+            url: `https://filipwojda.dev/blog/${post.slug}`,
             author: {
               '@type': 'Person',
-              name: 'Lee Robinson',
+              name: 'Filip Wojda',
             },
           }),
         }}
