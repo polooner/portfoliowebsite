47ee69a193c675a572bc0ddc708fd29f08c37cca
Filip Wojda <wojdafilipdev@gmail.com>
2024-04-14 21:50:57 -0400

    blog fixes

--- components/copy-button.tsx
+++ components/copy-button.tsx
@@ -1,40 +1,40 @@
-'use client';
+// 'use client'import { Check } from 'lucide-react';
 
-import { Check } from 'lucide-react';
-import { useState } from 'react';
-import { Button } from './ui/button';
+// export default function CopyButton({
+//   children,
+//   contentTemplate,
+// }: {
+//   children: React.ReactNode;
+//   contentTemplate: string;
+// }) {
+//   const [showCopied, setShowCopied] = useState(false);
+//   const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout | null>(
+//     null
+//   );
+//   function copy() {
+//     if (currentTimeout) clearTimeout(currentTimeout);
+//     navigator.clipboard.writeText(
+//       contentTemplate.replace('link', `${window.location.href}`)
+//     );
+//     setShowCopied(true);
+//     setCurrentTimeout(setTimeout(() => setShowCopied(false), 1000));
+//   }
+//   return (
+//     <button
+//       onClick={copy}
+//       className='flex flex-row justify-center gap-2 duration-150'
+//     >
+//       {showCopied ? (
+//         <>
+//           <Check size='18' /> Copied!
+//         </>
+//       ) : (
+//         <>{children}</>
+//       )}
+//     </button>
+//   );
+// }
 
-export default function CopyButton({
-  children,
-  contentTemplate,
-}: {
-  children: React.ReactNode;
-  contentTemplate: string;
-}) {
-  const [showCopied, setShowCopied] = useState(false);
-  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout | null>(
-    null
-  );
-  function copy() {
-    if (currentTimeout) clearTimeout(currentTimeout);
-    navigator.clipboard.writeText(
-      contentTemplate.replace('link', `${window.location.href}`)
-    );
-    setShowCopied(true);
-    setCurrentTimeout(setTimeout(() => setShowCopied(false), 1000));
-  }
-  return (
-    <Button
-      onClick={copy}
-      className='flex flex-row justify-center gap-2 duration-150'
-    >
-      {showCopied ? (
-        <>
-          <Check size='18' /> Copied!
-        </>
-      ) : (
-        <>{children}</>
-      )}
-    </Button>
-  );
+export default function CopyButton() {
+  return <div>idk</div>;
 }
