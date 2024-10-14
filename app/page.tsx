6c88ca2c3da59d5b7575f271efd350193053767a
Filip Wojda <wojdafilipdev@gmail.com>
2024-10-13 21:56:24 -0700

    update front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,49 @@
-import { Metadata } from 'next';
-import Link from 'next/link';
+import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Filip Wojda',
-  description: 'I like to build.',
+  title: "Filip Wojda",
+  description: "I like to build.",
 };
 
 export default function Home() {
   return (
-    <section className='flex flex-col gap-y-10 max-w-[500px]'>
-      <div className='inline-flex'>
-        hey, my name is <h2 className='font-bold'>&nbsp;Filip.</h2>
-      </div>
-      <div>I am a CS student and an indie-hacker based out of NYC.</div>
-      <div>
-        currently building{' '}
-        <Link
-          className='font-bold hover:underline'
-          target='_blank'
-          href='https://www.makeklips.ai'
-        >
-          makeklips.ai
-        </Link>
+    <section className="flex flex-col gap-y-10 max-w-[500px] py-20">
+      <div className=" text-start">
+        <strong>Filip Wojda</strong>
+        <br />
+
+        <p>ai engineer</p>
+
+        <br />
+        <br />
+        <div>
+          currently a founding engineer at{" "}
+          <Link
+            className="font-bold hover:underline"
+            target="_blank"
+            href="https://www.julius.ai"
+          >
+            julius.ai
+          </Link>
+        </div>
+        <div>
+          <br />
+          previously built{" "}
+          <Link
+            className="font-bold hover:underline"
+            target="_blank"
+            href="https://www.makeklips.ai"
+          >
+            makeklips.ai
+          </Link>
+        </div>
+        <br />
+        <p>i am based out of sf</p>
+        <br />
+        <div>
+          i like philosophy <br /> and ai <br /> and ui.
+        </div>
       </div>
     </section>
   );
