b72c9f494712e1933ead67ee423ccb8c5098cbfa
Filip Wojda <wojdafilipdev@gmail.com>
2024-11-09 23:18:14 -0800

    blogs

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import { CustomMDX } from 'app/components/mdx';
-import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
+import { CustomMDX } from "app/components/mdx";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 // import { getViewsCount } from 'app/db/queries';
-import { getBlogPosts } from 'app/db/blog';
+import { getBlogPosts } from "app/db/blog";
 // import { increment } from 'app/db/actions';
 
 export async function generateMetadata({
@@ -29,7 +29,7 @@ export async function generateMetadata({
     openGraph: {
       title,
       description,
-      type: 'article',
+      type: "article",
       publishedTime,
       url: `https://filipwojda.dev/blog/${post.slug}`,
       images: [
@@ -39,7 +39,7 @@ export async function generateMetadata({
       ],
     },
     twitter: {
-      card: 'summary_large_image',
+      card: "summary_large_image",
       title,
       description,
       images: [ogImage],
@@ -49,7 +49,7 @@ export async function generateMetadata({
 
 function formatDate(date: string) {
   let currentDate = new Date();
-  if (!date.includes('T')) {
+  if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
   let targetDate = new Date(date);
@@ -63,7 +63,7 @@ function formatDate(date: string) {
   let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
   let daysAgo = currentDate.getDate() - targetDate.getDate();
 
-  let formattedDate = '';
+  let formattedDate = "";
 
   if (yearsAgo > 0) {
     formattedDate = `${yearsAgo}y ago`;
@@ -72,13 +72,13 @@ function formatDate(date: string) {
   } else if (daysAgo > 0) {
     formattedDate = `${daysAgo}d ago`;
   } else {
-    formattedDate = 'Today';
+    formattedDate = "Today";
   }
 
-  let fullDate = targetDate.toLocaleString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
+  let fullDate = targetDate.toLocaleString("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   });
 
   return `${fullDate} (${formattedDate})`;
@@ -92,14 +92,14 @@ export default function Blog({ params }) {
   }
 
   return (
-    <section className='flex flex-col  items-center p-8'>
+    <section className="flex flex-col  items-center p-8">
       <script
-        type='application/ld+json'
+        type="application/ld+json"
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
             headline: post.metadata.title,
             datePublished: post.metadata.publishedAt,
             dateModified: post.metadata.publishedAt,
@@ -109,24 +109,24 @@ export default function Blog({ params }) {
               : `https://filipwojda.dev/og?title=${post.metadata.title}`,
             url: `https://filipwojda.dev/blog/${post.slug}`,
             author: {
-              '@type': 'Person',
-              name: 'Filip Wojda',
+              "@type": "Person",
+              name: "Filip Wojda",
             },
           }),
         }}
       />
-      <h1 className='title font-medium text-2xl tracking-tighter max-w-[650px]'>
+      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
         {post.metadata.title}
       </h1>
-      <div className='flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]'>
-        <p className='text-sm text-black'>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+        <p className="text-sm text-black">
           {formatDate(post.metadata.publishedAt)}
         </p>
         {/* <Suspense fallback={<p className='h-5' />}>
           <Views slug={post.slug} />
         </Suspense> */}
       </div>
-      <article className='prose text-black prose-quoteless prose-neutral dark:prose-invert'>
+      <article className="prose text-black prose-quoteless prose-neutral dark:prose-invert sm:p-10 md:p-20 lg:p-32">
         <CustomMDX source={post.content} />
       </article>
     </section>
