6f9d40913f15730e740e4e7900e9c13e6e76c382
Filip Wojda <wojdafilipdev@gmail.com>
2025-10-11 17:13:50 -0400

    lab

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -28,7 +28,7 @@ export default function NavBar({ children }: { children?: React.ReactNode }) {
           href="/lab"
           className="flex hover:bg-black text-black hover:text-white flex-row items-center px-4"
         >
-          <p>ui lab</p>
+          <p>lab</p>
         </Link>
         <Link
           href="/artworks"
