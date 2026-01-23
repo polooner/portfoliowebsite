aab6405a75bc89eaea1e63d94e194b17cb5847a3
Filip Wojda <wojdafilipdev@gmail.com>
2026-01-23 14:07:27 -0500

    build

--- components/design-experiments/header-grid/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { SectionHeader } from "./section-header";
-export { FontGrid } from "./font-grid";
-export { InkBleedPlayground } from "./ink-bleed-playground";
-export { TypefaceVariations } from "./typeface-variations";
-export { BlurInPlayground } from "./blur-in-playground";
