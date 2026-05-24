c50576e2872cbd786a3b88bb4de8145366383395
Filip Wojda <wojdafilipdev@gmail.com>
2026-05-24 14:51:51 -0400

    push

--- app/(main)/_components/site-header.tsx
+++ app/(main)/_components/site-header.tsx
@@ -5,7 +5,6 @@ export function SiteHeader() {
     <div className="flex flex-row items-start justify-between gap-4 max-w-[360px]">
       <h1 className="text-2xl leading-tight">
         Filip Wojda
-        <span className="block text-neutral-500">mutlficated creative</span>
       </h1>
       <div className="font-mono text-neutral-600 shrink-0">
         <Clock />
